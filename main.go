1ce922393c2344a3b6b0e7d9072449c4e33af165
Mike Woudenberg <mwoudenberg@xebia.com>
2017-09-08 13:45:45 +0200

    Adds jwt validation poc

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 	"github.com/stianeikeland/go-rpio"
 )
@@ -37,9 +39,48 @@ func main() {
 
 	router := chi.NewRouter()
 	router.HandleFunc("/toggle", toggleGate)
+	router.HandleFunc("/validate", validateJWT)
 
 	fmt.Printf("starting service listening  on port [%s]", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
+}
+
+func valid(req *http.Request) bool {
+	result := false
+	headToken := req.Header.Get("Authorization")
+	var tokenString string
+
+	if strings.Contains(headToken, "Bearer ") {
+		tokenString = headToken[7:len(headToken)]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+			return []byte("my_secret_keyy"), nil
+		})
+
+		fmt.Println(err)
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			beam, _ := claims["beam"].(string)
+			if beam == "yes" {
+				return true
+			}
+		}
+	}
+	return result
+}
+
+func validateJWT(w http.ResponseWriter, req *http.Request) {
+	if valid(req) {
+		w.Write([]byte("Proceed"))
+	} else {
+		w.WriteHeader(401)
+		w.Write([]byte("Please authenticate"))
+	}
 }
 
 func toggleGate(w http.ResponseWriter, req *http.Request) {
